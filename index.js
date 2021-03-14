6d5c58a8357d32d1e9c5762686e7a73619403c1c
Peniel <dialundamapeniel@gmail.com>
2021-03-14 22:38:01 +0100

    fix port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express")
 const app = express();
 // const server = require("http").Server(app)
 const { config } = require("process");
-const PORT = process.env.port || 5001;
+const PORT = process.env.PORT || 5001;
 
 const allUser = {};
 
